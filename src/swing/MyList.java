9bcdeffc7831ad9f8cf6c1b560f0b0ce52944cdb
Anatoli Grishenko <Anatoli.Grishenko@gmail.com>
2022-02-23 13:10:17 +0100

    Added support to list of [Strings, Filename, Foldername]
    All of them have their own buttons to insert/delete elements and use specific subdialogs to input lines, or select filenames, foldernames

---
+++ src/swing/MyList.java
@@ -0,0 +1,151 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package swing;
+
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.ListSelectionModel;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+/**
+ *
+ * @author Anatoli Grishenko <Anatoli.Grishenko@gmail.com>
+ */
+public class MyList extends JList implements ListSelectionListener, ActionListener {
+
+    public static enum Type {
+        STRING, FILE, FOLDER
+    };
+    private JButton add, remove;
+    private DefaultListModel listModel;
+    private JScrollPane listPane;
+    private Type mytype;
+
+    public MyList() {
+        super();
+    }
+
+    public MyList init(Type t) {
+        listModel = new DefaultListModel();
+        add = new JButton("+");
+        add.addActionListener(this);
+        remove = new JButton("-");
+        remove.addActionListener(this);
+        mytype = t;
+        this.setModel(listModel);
+        setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+        addListSelectionListener(this);
+        setVisibleRowCount(3);
+        listPane = new JScrollPane(this);
+        listPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        listPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+        listPane.setPreferredSize(new Dimension(100,100));
+        return this;
+    }
+
+    public MyList addElement(String element) {
+        listModel.addElement(element);
+        return this;
+    }
+
+    public MyList addAllElements(ArrayList<String> elements) {
+        for (String element : elements) {
+            listModel.addElement(element);
+        }
+        return this;
+    }
+
+    public MyList removeElement(int pos) {
+        listModel.remove(pos);
+        return this;
+    }
+
+    public MyList removeElement(String element) {
+        listModel.removeElement(element);
+//        int index = this.gets
+//        listModel.addElement(element);
+        return this;
+    }
+
+    private int getIndex(String what) {
+        int res = -1;
+        for (int i = 0; i < listModel.size(); i++) {
+            if (what.equals((String) listModel.get(i))) {
+                return i;
+            }
+        }
+        return res;
+    }
+
+    public DefaultListModel getListMode() {
+        return listModel;
+    }
+
+    public JScrollPane getPane() {
+        return listPane;
+    }
+
+    public JButton getAddButton() {
+        return add;
+    }
+
+    public JButton getRemoveButton() {
+        return remove;
+    }
+
+    @Override
+    public void valueChanged(ListSelectionEvent e) {
+        if (e.getValueIsAdjusting() == false) {
+            if (getSelectedIndex() == -1) {
+                remove.setEnabled(false);
+
+            } else {
+                remove.setEnabled(true);
+            }
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String toAdd = "";
+        switch (e.getActionCommand()) {
+            case "+":
+                switch (mytype) {
+                    case FILE:
+                        toAdd= OleDialog.doSelectFile("./");
+                        break;
+                    case FOLDER:
+                        toAdd= OleDialog.doSelectFolder("./");
+                        break;
+                    case STRING:
+                        toAdd = JOptionPane.showInputDialog(null, "Please type new string to add", "List", JOptionPane.QUESTION_MESSAGE);
+                        break;
+                }
+                if (toAdd != null && toAdd.length() > 0 && !listModel.contains(toAdd)) {
+                    int index=listModel.size() + 1;
+                    listModel.addElement(toAdd);
+                    this.setSelectedIndex(listModel.size()-1);
+                    this.ensureIndexIsVisible(listModel.size()-1);
+                }
+                break;
+            case "-":
+                int index = this.getSelectedIndex();
+                this.removeElement(index);
+                break;
+        }
+    }
+
+}
--- src/swing/OleDialog.java
+++ src/swing/OleDialog.java
@@ -5,6 +5,7 @@
  */
 package swing;
 
+import com.eclipsesource.json.JsonArray;
 import data.Ole;
 import data.Ole.oletype;
 import data.OleConfig;
@@ -23,6 +24,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.io.File;
+import java.nio.file.FileSystems;
+import static java.nio.file.FileSystems.getDefault;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -32,6 +35,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
@@ -39,13 +43,18 @@ import javax.swing.JDialog;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
 import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import static swing.SwingTools.doSwingLater;
 import static swing.SwingTools.doSwingWait;
 
@@ -54,6 +63,44 @@ import static swing.SwingTools.doSwingWait;
  * @author Anatoli Grishenko Anatoli.Grishenko@gmail.com
  */
 public class OleDialog extends JDialog implements ActionListener {
+
+    protected static String doSelectFile(String currentfolder) {
+        JFileChooser choose;
+
+        String currentpath = Paths.get("").toAbsolutePath().toString(),
+                filepath = FileSystems.getDefault().getPath(currentfolder).normalize().toAbsolutePath().toString();
+        choose = new JFileChooser();
+        choose.setCurrentDirectory(new File(currentpath));
+        choose.setDialogTitle("Please select a file");
+        choose.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        if (choose.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+            String absolutefile = choose.getSelectedFile().toURI().getPath().toString(),
+                    relative = new File(currentpath).toURI().relativize(new File(absolutefile).toURI()).getPath();
+            return relative;
+        } else {
+            return null;
+        }
+
+    }
+
+    protected static String doSelectFolder(String currentfolder) {
+        JFileChooser choose;
+
+        String currentpath = Paths.get("").toAbsolutePath().toString(),
+                filepath = FileSystems.getDefault().getPath(currentfolder).normalize().toAbsolutePath().toString();
+        choose = new JFileChooser();
+        choose.setCurrentDirectory(new File(currentpath));
+        choose.setDialogTitle("Please select a folder");
+        choose.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        if (choose.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+            String absolutefile = choose.getSelectedFile().toURI().getPath().toString(),
+                    relative = new File(currentpath).toURI().relativize(new File(absolutefile).toURI()).getPath();
+            return relative;
+        } else {
+            return null;
+        }
+
+    }
 
     OleConfig output, input;
     HashMap<String, Component> components;
@@ -105,27 +152,27 @@ public class OleDialog extends JDialog implements ActionListener {
                 if (e.getActionCommand().startsWith(".../")) { //select folder
                     sfield = e.getActionCommand().replace(".../", "");
                     if (components.get(sfield) != null) {
-                        choosevalue = (JTextField) components.get(sfield);                        
-                        choose = new JFileChooser();
-                        String cwd=Paths.get("").toAbsolutePath().toString()+"/"+(choosevalue.getText().length()>0?choosevalue.getText(): "./");
-                        choose.setCurrentDirectory(new File(cwd));
-                        choose.setDialogTitle("Please select "+sfield);
-                        choose.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-                        if (choose.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
-                            choosevalue.setText(choose.getSelectedFile().getName());                            
+                        choosevalue = (JTextField) components.get(sfield);
+                        String res = this.doSelectFolder(choosevalue.getText().length() > 0 ? choosevalue.getText() : "./");
+                        if (res != null) {
+                            choosevalue.setText(res);
                         }
                     }
                 } else if (e.getActionCommand().startsWith("...")) { //select file
                     sfield = e.getActionCommand().replace("...", "");
                     if (components.get(sfield) != null) {
-                        choosevalue = (JTextField) components.get(sfield);                        
+                        choosevalue = (JTextField) components.get(sfield);
                         choose = new JFileChooser();
-                        String cwd=Paths.get("").toAbsolutePath().toString();
-                        choose.setCurrentDirectory(new File(cwd));
-                        choose.setDialogTitle("Please select "+sfield);
+                        String currentpath = Paths.get("").toAbsolutePath().toString(),
+                                filepath = FileSystems.getDefault().getPath(choosevalue.getText()).normalize().toAbsolutePath().toString();
+//                        String cwd = Paths.get("").toAbsolutePath().toString();
+                        choose.setCurrentDirectory(new File(currentpath));
+                        choose.setDialogTitle("Please select " + sfield);
                         choose.setFileSelectionMode(JFileChooser.FILES_ONLY);
                         if (choose.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
-                            choosevalue.setText(choose.getSelectedFile().getName());                            
+                            String absolutefile = choose.getSelectedFile().getAbsolutePath().toString(),
+                                    relative = new File(currentpath).toURI().relativize(new File(absolutefile).toURI()).getPath();
+                            choosevalue.setText(relative);
                         }
                     }
                 }
@@ -156,6 +203,7 @@ public class OleDialog extends JDialog implements ActionListener {
         JTextField text;
         JCheckBox checkbox;
         JComboBox combobox;
+        MyList list;
         Ole fieldproperties, panelproperties;
         String tooltip;
         ArrayList<String> select;
@@ -172,17 +220,25 @@ public class OleDialog extends JDialog implements ActionListener {
         gc.weighty = 1;
         gc.insets = new Insets(spacing, spacing, spacing, spacing);
         gc.anchor = GridBagConstraints.NORTHWEST;
-//        gc.fill = GridBagConstraints.HORIZONTAL;
+        gc.fill = GridBagConstraints.HORIZONTAL;
         gc.gridx = 0;
         gc.gridy = 0;
-        gc.gridwidth = columns; // Columns?
+        gc.gridwidth = 1; // Columns?
+        gc.gridheight = 1;
         if (panelproperties.getBoolean("border", false)) {
             dataPanel.setBorder(BorderFactory.createTitledBorder(oid));
         }
         for (String sfield : ocomponents.getFieldList()) {
             fieldproperties = input.getProperties(sfield);
             gc.gridwidth = input.getProperties(sfield).getInt("columns", 1);
-            if (ocomponents.getFieldType(sfield).equals(oletype.INTEGER.name())
+            if (ocomponents.getField(sfield).contains("<html>")) {
+//                gc.gridx = 0;
+                gc.gridwidth = GridBagConstraints.REMAINDER;
+                label = new JLabel(ocomponents.getField(sfield));
+                dataPanel.add(label, gc);
+                gc.gridx = columns;
+                gc.gridwidth = 1; // Columns?
+            } else if (ocomponents.getFieldType(sfield).equals(oletype.INTEGER.name())
                     || ocomponents.getFieldType(sfield).equals(oletype.DOUBLE.name())
                     || ocomponents.getFieldType(sfield).equals(oletype.STRING.name())) {
                 label = new JLabel(sfield);
@@ -262,6 +318,33 @@ public class OleDialog extends JDialog implements ActionListener {
                 gc.gridx++;
                 dataPanel.add(checkbox, gc);
                 gc.gridx++;
+            } else if (ocomponents.getFieldType(sfield).equals(oletype.ARRAY.name())) {
+                int listsize = 3;
+                MyList.Type listtype;
+                try {
+                    listtype = MyList.Type.valueOf(fieldproperties.getString("listtype").toUpperCase());
+                }catch(Exception ex) {                    
+                    listtype= MyList.Type.STRING;
+                }
+                list = new MyList().init(listtype);
+                list.addAllElements(ocomponents.getArray(sfield));
+                list.setVisibleRowCount(listsize);
+                tooltip = fieldproperties.getString("tooltip");
+                if (tooltip != null) {
+                    list.setToolTipText(tooltip);
+                }
+                components.put(sfield, list);
+                label = new JLabel(sfield);
+                dataPanel.add(label, gc);
+                gc.gridx++;
+                gc.gridheight = listsize;
+                dataPanel.add(list.getPane(), gc);
+                gc.gridx++;
+                gc.gridheight = 1;
+                dataPanel.add(list.getAddButton(), gc);
+                gc.gridy++;
+                dataPanel.add(list.getRemoveButton(), gc);
+                gc.gridy += listsize;
             } else if (ocomponents.getFieldType(sfield).equals(oletype.OLE.name())) {
                 gc.gridx = 0;
                 dataPanel.add(addToLayout(sfield, ocomponents.getOle(sfield)), gc);
@@ -305,10 +388,14 @@ public class OleDialog extends JDialog implements ActionListener {
         JCheckBox checkbox;
         ArrayList<String> select;
         JComboBox combobox;
+        MyList list;
         Ole fieldproperties;
 
         for (String sfield : currentTab.getFieldList()) {
             fieldproperties = input.getProperties(sfield);
+            if (currentTab.getField(sfield).contains("<html>")) {
+                continue;
+            }
             select = fieldproperties.getArray("select");
             if (currentTab.getFieldType(sfield).equals(oletype.STRING.name())) {
                 if (select == null) {
@@ -337,6 +424,15 @@ public class OleDialog extends JDialog implements ActionListener {
             } else if (currentTab.getFieldType(sfield).equals(oletype.BOOLEAN.name())) {
                 checkbox = (JCheckBox) components.get(sfield);
                 currentTab.setField(sfield, checkbox.isSelected());
+            } else if (currentTab.getFieldType(sfield).equals(oletype.ARRAY.name())) {
+                ArrayList<String> sList=new ArrayList();
+                JsonArray jaarray = new JsonArray();
+                MyList mlist = (MyList) components.get(sfield);
+                DefaultListModel mlm = mlist.getListMode();
+                for (int i = 0; i < mlm.size(); i++) {
+                    sList.add((String)mlm.getElementAt(i));
+                }
+                currentTab.setField(sfield, new ArrayList(sList));
             } else if (currentTab.getFieldType(sfield).equals(oletype.OLE.name())) {
                 getFromLayout(currentTab.getOle(sfield));
             }
@@ -354,4 +450,16 @@ public class OleDialog extends JDialog implements ActionListener {
         }
     }
 
+//    protected String doSelectFolder(String currentfile) {
+//        JFileChooser choose = new JFileChooser();
+//        String cwd = Paths.get("").toAbsolutePath().toString() + "/" + (currentfolder.length() > 0 ? currentfolder : "./");
+//        choose.setCurrentDirectory(new File(cwd));
+//        choose.setDialogTitle("Please select a folder");
+//        choose.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+//        if (choose.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
+//           return choose.getSelectedFile().getName();
+//        } else
+//            return null;
+//
+//    }
 }
